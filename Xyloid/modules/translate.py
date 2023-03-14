262fb2f1f559f8f720c127a674ed2198911b4e9a
xyloid <125530225+axallll@users.noreply.github.com>
2023-03-14 13:29:53 +0700

    Update and rename Kazu/modules/translate.py to Xyloid/modules/translate.py

--- Xyloid/modules/translate.py
+++ Xyloid/modules/translate.py
@@ -12,7 +12,7 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
 from config import CMD_HANDLER as cmd
-from Kazu.helpers.basic import edit_or_reply
+from Xyloid.helpers.basic import edit_or_reply
 
 from .help import add_command_help
 
