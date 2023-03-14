ecf613b47d68209c0e53a6d76efd3a1183dcf5f5
xyloid <125530225+axallll@users.noreply.github.com>
2023-03-14 09:05:28 +0700

    Update help.py

--- Xyloid/modules/help.py
+++ Xyloid/modules/help.py
@@ -4,10 +4,10 @@ from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
-from Kazu import app, CMD_HELP
-from Kazu.helpers.cmd import *
-from Kazu.helpers.PyroHelpers import ReplyCheck
-from Kazu.helpers.utility import split_list
+from Xyloid import app, CMD_HELP
+from Xyloid.helpers.cmd import *
+from Xyloid.helpers.PyroHelpers import ReplyCheck
+from Xyloid.helpers.utility import split_list
 
 
 async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "PyroZu-UserBot Plugins"
+            ac.title = "Xyloid-UserBot Plugins"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @kazusupportgrp × @Html12text •",
+                f"```{str(ac)}```\n• @xyloidsupport × @pscsb0yz •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Html12text"
+            this_command += "© @pscsb0yz"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -80,12 +80,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroZu-UserBot Plugins"
+        ac.title = "Xyloid-UserBot Plugins"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @kazusupportgrp × @Html12text •"
+            message, f"```{str(ac)}```\n• @xyloidsupport × @pscsb0yz •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Html12text"
+            this_command += "© @pscsb0yz"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
